d2e542f0e360756de30323c3edd7932ee0ba3a1d
Vighnesh Shenekar <115893429+vighnxsh@users.noreply.github.com>
2024-07-13 12:45:02 +0530

    feed updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
-import { get } from "http";
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
-import { getAuthSession } from "@/lib/auth";
 
 export async function middleware(req: NextRequest) {
-  const session = await getAuthSession();
+  const token = await getToken({ req });
+  const session = token ? token.session : null;
   if (!session) {
     return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
   }

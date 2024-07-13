0bb49648ed01bc4c766347d431fef20600f9fc30
Vighnesh Shenekar <115893429+vighnxsh@users.noreply.github.com>
2024-07-13 11:07:14 +0530

    root page changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import './page.css'
 import { SkeletonCard } from "@/components/ui/SkeletonCard";
 import { db } from "@/lib/db";
 import { Divide } from "lucide-react";
+import SignIn from "@/components/SignIn";
 
 
 export const dynamic = "force-dynamic";
@@ -20,7 +21,8 @@ export default async function Home() {
  
   return (
     <>
-    <p className=" text-transparent bg-clip-text z-10 text text-5xl bg-gradient-to-r from-violet-100 via-emerald-200  to-indigo-700 font-bold">Your feed ~</p>
+    {session ?     <p className=" text-transparent bg-clip-text z-10 text text-5xl bg-gradient-to-r from-violet-100 via-emerald-200  to-indigo-700 font-bold">Your feed ~</p>
+: <p className="text text-transparent bg-clip-text z-10  text-3xl bg-gradient-to-r from-violet-100 via-emerald-200  to-indigo-700 font-bold">Sign in for customized feed~</p>}
      
       <div className='grid grid-cols-2 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
       

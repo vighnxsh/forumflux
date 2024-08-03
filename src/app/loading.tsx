5b3f9086c0619294c4b96fbd768a84a87cb34745
Vighnesh Shenekar <115893429+vighnxsh@users.noreply.github.com>
2024-08-03 15:25:27 +0530

    loading page added

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+// app/loading.js
+export default function Loading() {
+    return (
+      <div className="loading-screen">
+        <div className="spinner"></div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+  
